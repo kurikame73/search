5e3a86f9dc39f0f58bef4b118729547dae1ddeb1
makisekurisu <kurikame1017@gmail.com>
2024-09-07 19:05:03 +0900

    카프카 설정 추가 ////////////////////// 필요없는 이벤트 제거 /////////////////////// 재고 감소 메세지 수신 후 인덱스 업데이트

--- src/main/java/com/example/demo/ItemSearchEntity/ElasticsearchConfig.java
+++ src/main/java/com/example/demo/ItemSearchEntity/ElasticsearchConfig.java
@@ -6,7 +6,6 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 
 @Configuration
 @Slf4j
